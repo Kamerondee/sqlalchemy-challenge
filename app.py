83e7c5884f4768ff346f0e20bc6e032db59f749e
Karam Deol <kdeol91@gmail.com>
2022-06-07 21:37:29 -0500

    flask routes

--- app.py
+++ app.py
@@ -13,3 +13,38 @@ Base = automap_base()
 
 # Reflect the tables
 Base.prepare(engine, reflect=True)
+
+# Save references to each table
+measurement = Base.classes.measurement
+station = Base.classes.station
+
+# Set up Flask
+app = Flask(__name__)
+
+
+# Flask Routes
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Welcome to the SQL-Alchemy APP API!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/[start_date format:yyyy-mm-dd]<br/>"
+        f"/api/v1.0/[start_date format:yyyy-mm-dd]/[end_date format:yyyy-mm-dd]<br/>"
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    # Create session from Python to the database
+    session = Session(engine)
+
+    """Return a list of all Precipitation Data"""
+    # Query all Precipitation data
+    results = session.query(measurement.date, measurement.prcp).filter(measurement.date >= "2016-08-24").all()
+
+    session.close()
+
